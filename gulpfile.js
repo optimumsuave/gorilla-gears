c22f2ef7b2a8349022518b69940d7915d18ac013
Connor <connor@420connect.com>
2016-02-09 21:02:43 -0800

    added gulp stuff and other necessary files

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,14 @@ var rename  = require('gulp-rename');
 var uglify  = require('gulp-uglify');
  
 gulp.task('sass', function () {
-  	gulp.src('assets/scss/main.scss')
-    	.pipe(sass({
-      	errLogToConsole: true,
-      	style: 'compress',
-      	includePaths: require('node-bourbon').includePaths
-    	}))
-    	.pipe(gulp.dest('assets/css'));
+	gulp.src('assets/scss/main.scss')
+  	.pipe(sass({
+    	errLogToConsole: true,
+    	style: 'compress',
+      outputStyle: 'compressed',
+    	includePaths: require('node-bourbon').includePaths
+  	}))
+  	.pipe(gulp.dest('assets/css'));
 });
 
 gulp.task('compress', function() {
@@ -22,7 +23,7 @@ gulp.task('compress', function() {
 });
 
 gulp.task('watch', function() {
-	gulp.watch('assets/scss/*.scss', function() {
+  gulp.watch('assets/scss/*.scss', function() {
     gulp.start('sass');
   });
   gulp.watch('assets/js/src/app.js', function() {
